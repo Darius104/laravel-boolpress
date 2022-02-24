c7b99889dd0735549dd56b57277e7ba583e3434f
Darius104 <comandarius@gmail.com>
2022-02-24 20:57:00 +0100

    Scaffolding Migrate + seeder && added category id to post table

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1>Salve</h1>
+    <h1>Salve {{ $user->name }} nell'area protetta</h1>
+
+    @if ($user->userInfo)
+        <div>Numero di telefono: {{ $userInfo->phone }} </div>
+        <div>Indirizzo: {{ $userInfo->adress }} </div>
+    @endif
 @endsection
