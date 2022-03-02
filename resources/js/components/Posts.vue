3f3eccccb68b72133e3c4121b00d1670036d5111
Darius104 <comandarius@gmail.com>
2022-03-02 15:56:45 +0100

    Post component scaffolding created

---
+++ resources/js/components/Posts.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>
+        <h1>lista dei posts</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Posts'
+}
+</script>
--- resources/js/components/WorkInProgress.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1>work in progress...</h1>
-</template>
-
-<script>
-export default {
-    name: 'WorkInProgress'
-}
-</script>
