8dab87e9a91734df97c5e7a9cba018d14c7f7841
Darius104 <comandarius@gmail.com>
2022-03-02 16:39:11 +0100

    Posts vue and PostController Done

--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -1,11 +1,51 @@
 <template>
-    <section>
-        <h1>lista dei posts</h1>
-    </section>
+  <section>
+    <div class="container">
+      <h1>I nostri post</h1>
+      <!-- SINGLE CARD -->
+      <div class="row row-cols-3">
+        <div v-for="post in posts" :key="post.id" class="col">
+          <div class="card my-3">
+            <!-- <img src="..." class="card-img-top" alt="..." /> -->
+            <div class="card-body">
+              <h5 class="card-title">{{ post.title }}</h5>
+              <p class="card-text">
+                {{ cutText(post.content, 50) }}
+              </p>
+              <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            </div>
+          </div>
+        </div>
+        <!-- END SINGLE CARD -->
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    name: 'Posts'
-}
+  name: 'Posts',
+  data: function () {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    getPosts: function () {
+      axios.get('/api/posts').then((response) => {
+        this.posts = response.data.results;
+      });
+    },
+    cutText: function (text, maxCharsNumber) {
+      // taglia il testo se è più lungo di 50 caratteri e aggiunge 3 punti alla fine
+      if (text.length > maxCharsNumber) {
+        return text.substr(0, maxCharsNumber) + '...';
+      }
+      return text;
+    },
+  },
+  created: function () {
+    this.getPosts();
+  },
+};
 </script>
