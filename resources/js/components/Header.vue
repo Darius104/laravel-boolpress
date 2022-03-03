9ea21f65c63bf176a71f705123c8bc3be682f851
Darius104 <comandarius@gmail.com>
2022-03-03 15:49:43 +0100

    Last push

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <span class="navbar-brand" href="#">Boolpress</span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li v-for="(element, index) in menu" :key="index" class="nav-item active">
+                        <router-link class="nav-link" :to="{ name: element.name }"> {{ element.label }} </router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data: function() {
+        return{
+            menu: [
+                {
+                    name: 'home',
+                    label: 'Home'
+                },
+                {
+                    name: 'about',
+                    label: 'About'
+                },
+                {
+                    name: 'blog',
+                    label: 'Blog'
+                }
+            ],
+        }
+    }
+}
+</script>
